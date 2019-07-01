bea57fed0a6b768613e3755236b92f8a839ada62
zinyando <brizdigital@gmail.com>
2019-07-01 10:16:02 +0200

    Adds game cell accepatance test

--- tests/integration/components/game-cell-test.js
+++ tests/integration/components/game-cell-test.js
@@ -1,26 +1,83 @@
-import { module, skip } from 'qunit';
+import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { setupMirage } from 'ember-cli-mirage/test-support';
 
 module('Integration | Component | game-cell', function(hooks) {
   setupRenderingTest(hooks);
+  setupMirage(hooks);
 
-  skip('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('it has a dark gray colour when it has a ship', async function(assert) {
+    let cell = this.server.create('cell', {
+      row: 1,
+      column: 1,
+      hasShip: true,
+      shipName: 'A'
+    });
+    
+    this.set('cell', cell)
 
-    await render(hbs`{{game-cell}}`);
-
-    assert.equal(('' + this.element.textContent).trim(), '');
-
-    // Template block usage:
     await render(hbs`
-      {{#game-cell}}
-        template block text
-      {{/game-cell}}
+      <GameCell @cell={{cell}} @muskCell={{false}} />
     `);
 
-    assert.equal(('' + this.element.textContent).trim(), 'template block text');
+    assert.ok(this.element.querySelector('.bg-gray-800'), 'The cell has a ship');
+  });
+
+  test('it has a blue colour when it does not have a ship', async function(assert) {
+    let cell = this.server.create('cell', {
+      row: 1,
+      column: 1,
+      hasShip: false,
+      shipName: 'A'
+    });
+    
+    this.set('cell', cell)
+
+    await render(hbs`
+      <GameCell @cell={{cell}} @muskCell={{false}} />
+    `);
+
+    assert.ok(this.element.querySelector('.bg-blue-300'), 'The cell has no ship assigned');
+  });
+
+  test('it has a white background when uskCell is true', async function(assert) {
+    let cell = this.server.create('cell', {
+      row: 1,
+      column: 1,
+      hasShip: false,
+      shipName: 'A'
+    });
+    
+    this.set('cell', cell)
+
+    await render(hbs`
+      <GameCell @cell={{cell}} @muskCell={{true}} />
+    `);
+
+    assert.ok(this.element.querySelector('.bg-white'), 'The cell is covered');
+  });
+
+
+  test('a musked cell changes colour when clicked', async function(assert) {
+    let cell = this.server.create('cell', {
+      row: 1,
+      column: 1,
+      hasShip: false,
+      shipName: 'A'
+    });
+    
+    this.set('cell', cell)
+
+    await render(hbs`
+      <GameCell @cell={{cell}} @muskCell={{true}} />
+    `);
+
+    assert.ok(this.element.querySelector('.bg-white'), 'The cell is covered');
+
+    await click('td.bg-white');
+
+    assert.ok(this.element.querySelector('.bg-blue-300'), 'The cell is now uncovered');
   });
 });
